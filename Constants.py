33aa7988fd6628e0b24221408f78a361b4b45390
Magnus Haw <magnus.haw@gmail.com>
2019-06-21 11:00:49 -0700

    added Constants

---
+++ Constants.py
@@ -0,0 +1,15 @@
+#Constants (SI Units)
+from numpy import pi
+
+mu0= pi*4e-7 #V s/(A m)
+c =  299792458. #m/s
+eps0 = 8.854187817e-12 #A s/(V m)
+h = 6.62606957e-34 #J s
+hbar = h/(2*pi)    #J s
+elec = 1.602176565e-19 #C
+amu = 1.660538921e-27 #kg
+mass_elec = 9.10938215e-31 #kg
+
+Kb = 1.3806488e-23
+Avogadro = 6.02214129e23
+AU = 149597870700. #m
