fba297875be13852267b17e23f2b621a6a6abb2f
joshq <joshqmorgan@gmail.com>
2026-03-25 10:29:23 -0700

    Functioning A-field solver

--- src/wireflux/physics/vector_potential.py
+++ src/wireflux/physics/vector_potential.py
@@ -53,7 +53,52 @@ def make_grid(boundaries,granularity=50,coordSys='cylindrical'):
     return grid
 
 
-def compute_potential_from_wire(p, I, path, delta=2., beta=1.e5):
+def meshgrid2pointlist(Q1,Q2,Q3):
+    """
+    Converts a set of fleshed out meshgrids to a list of points in 3D, arranged with columns q1, q2, q3. Helper function 
+    for methods which need to work with meshgrids for computation but show things as pyVista PointSets.
+    Point list must be converted to PointSet with another method.
+
+    Parameters:
+    -----------
+    Q1 : ndarray
+        Meshgrid of dimension l*m*n, in principal axis 1. Ideally l==m==n.
+    Q1 : ndarray
+        Meshgrid of dimension l*m*n, in principal axis 0. Ideally l==m==n.
+    Q1 : ndarray
+        Meshgrid of dimension l*m*n, in principal axis 2. Ideally l==m==n.
+
+    Returns
+    -------
+    pointlist : ndarray, shape (l*m*n,3)
+        List of 3D points, not sequentially organized.
+    """
+    l = Q1.shape[0]
+    m = Q1.shape[1]
+    n = Q1.shape[2]
+    
+    pointlist = np.zeros((l*m*n,3))   # Linear list of all points in grid
+
+    if ((l == m) and (m == n)):      # cubic matrix
+        for i in range(l):
+            for j in range(l):
+                for k in range(l):
+                    q1space = Q1[i,j,:]      
+                    q2space = Q2[i,j,:]
+                    q3space = Q3[k,k,:]
+        
+                    # Add l points in to the pointlist, ordered along index of diagonal, with colums q1,q2,q3
+                    pointlist[(i*l+j)*l : (i*l+(j+1))*l,:] = np.stack((q1space,q2space,q3space),
+                                                        axis=-1)
+    
+    else:        # Non-cubic matrix, harder to deal with...
+        print("Non-square matrix!") # TODO: Implement an actual method -JQM20260324
+        return None
+    
+    return pointlist
+
+
+def compute_potential_from_wire(p, I, path, r=0.25, delta=2., beta=1.e5):
     """
     Segment-based integral of current density (normalized units), to find magnetic vector potential at point p
     from given path.
@@ -66,6 +111,8 @@ def compute_potential_from_wire(p, I, path, delta=2., beta=1.e5):
         Current (normalized).
     path : array_like, shape (N, 3)
         Wire node positions.
+    r : float
+        Radius of the wire 
     delta : float
         Regularization length scale (normalized).
 
@@ -82,15 +129,16 @@ def compute_potential_from_wire(p, I, path, delta=2., beta=1.e5):
     mid = 0.5 * (path[1:] + path[:-1])     # (N-1, 3)
 
     # Vectors from segment midpoints to observation point
-    r = mid - p                            # (N-1, 3)
-    rmag = linalg.norm(r, axis=1)
+    rho = mid - p                            # (N-1, 3)
+    rhomag = np.linalg.norm(rho, axis=1)
 
-    # Regularization to avoid singularity
-    mask = rmag > delta
-    rmag = np.where(mask, rmag, np.inf)
-
+    # Regularization to avoid singularity, plus returning A=0 if point is 'within' wire within some error
+    if np.any(rhomag - np.sqrt( np.square(dl/2) + np.square(r*np.ones(dl.shape)) )): return np.array([0,0,0])   # If any point is within the wire, do not compute A TODO: This is an incomplete calculation, which overestimates the 'in the wire' part... adding r and dl/2 in quadrature means any point within a sphere of radius r+dl/2 is considered 'inside' the wire, even though the real requirement is that rho < r*sin(theta) + dl*cos(theta)/2, where theta is the angle between rho and dl--I just haven't actually written that out yet, and will rely on this overestimation for now -JQM20260325
+    mask = rhomag > delta
+    rhomag = np.where(mask, rhomag, np.inf)
+    
     # Integral sum of currents (normalized kernel) for coulomb gauge
-    dA = dl / (rmag[:,newaxis])
+    dA = dl / (rhomag[:,np.newaxis])
     A = np.sum(dA, axis=0)
 
     # Normalization & physical scaling
@@ -131,10 +179,39 @@ def getAField_fromWires(p, wires, beta=0.):
         
         Acontrib[i,:] = compute_potential_from_wire(p, I, path, delta=delta) 
 
-    A = np.sum(Acontrib,axis=1)   # NOTE: For now, I am not vectorizing this computation because I am not computing A along a path, rather computing A at many points on a 3D grid--I'm not sure if it's worth it to vectorize the computation for a large meshgrid, for memory purposes... -JQM20260317
+    A = np.sum(Acontrib,axis=0)   # NOTE: For now, I am not vectorizing this computation because I am not computing A along a path, rather computing A at many points on a 3D grid--I'm not sure if it's worth it to vectorize the computation for a large meshgrid, for memory purposes... -JQM20260317
     
     return A
+
+
+def getAField_onWire(p, wires):
+    """
+    Compute the magnetic vector potential along the path of a wire, from all wires outside of it. This is used to 
+    compute the magnetic linkage between a collection of wires, and should only be 
+
+    Parameters:
+    -----------
+    p : ndarray, shape (M, 3)
+        Path of test wire.
+    wires : list
+        Objects with attributes:
+            - p : ndarray, shape (N, 3)   (wire nodes)
+            - I : float                  (current)
+            - r : float                  (regularization length)
+
+    Returns
+    -------
+    A : ndarray, shape (m,3)
+        Magnetic field at all points along the path (physical units).
+    """
+    A = np.zeros(p.shape)
+    m = p.shape[0]
     
+    for i in range(m):
+        obspt = p[i,:]
+        A[i,:] = getAField_fromWires(obspt,wires)
+    
+    return A
 
 def laplaceSolver(grid, boundaries, boundaryConditions='dirichlet'):
     """
